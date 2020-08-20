0bfadc8af0733d1768cd94e531d9b2aefd164ab2
t.czysz <t.czysz@me.com>
2020-08-20 17:10:21 +0200

    commit

---
+++ MediaConverter/ViewModels/MainWindowModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaConverter
+{
+   public class MainWindowModel
+    {
+        #region Locals
+
+        #endregion Locals
+        #region Events
+
+        #endregion Events
+        #region Constructor
+
+        #endregion Constructor
+        #region Properties
+
+        #endregion Properties
+        #region Commands
+
+        #endregion Commands
+        #region Methods
+
+        #endregion Methods
+        #region Eventhandler
+
+        #endregion Eventhandler
+    }
+}
