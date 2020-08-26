181c31562359aed3dad126f0674588bb2296fe70
t.czysz <t.czysz@me.com>
2020-08-26 10:01:01 +0200

    MWM mit comments

--- MediaConverter/ViewModels/MainWindowModel.cs
+++ MediaConverter/ViewModels/MainWindowModel.cs
@@ -24,27 +24,30 @@ namespace MediaConverter
 
         #region Constructor
         public MainWindowModel()
-        {
+        {   
+            //Selections the user can choose (recover, convert, check or read logfile) in a treeview
+            //Auswahl, die der Nutzer wählen kann in einer Treeview
+            
             List<Tree> treeList = new List<Tree>();
-            //Zweig Wiederherstellen und reparieren 
-            Tree recover = new Tree() { Headline = "Wiederherstellen und reparieren" };     //Überschrift des Zweiges
-            recover.Nodes.Add(new TreeNode() { Select = "Original wiederherstellen" });         // Unterkategorie (Auswahl zum anklicken)
+            //Treenode recover and repair   //Zweig wiederherstellen und reparieren
+            Tree recover = new Tree() { Headline = "Wiederherstellen und reparieren" };     //Headlines of the treenodes 
+            recover.Nodes.Add(new TreeNode() { Select = "Original wiederherstellen" });         //Treenodes (subselections to click)
             recover.Nodes.Add(new TreeNode() { Select = "Belegseiten reparieren" });
             treeList.Add(recover);
 
-            //Zweig Konvertieren
+            //Treenode convert file         //Zweig konvertieren
             Tree convert = new Tree() { Headline = "Konvertieren" };
             convert.Nodes.Add(new TreeNode() { Select = "mit DB-Eintrag" });
             convert.Nodes.Add(new TreeNode() { Select = "mit DB-Eintrag" });
             treeList.Add(convert);
 
-            //Zweig Ablagecode prüfen
+            //Treenode check file           //Zweig prüfen
             Tree check = new Tree() { Headline = "Ablagecode prüfen" };
             check.Nodes.Add(new TreeNode() { Select = "für Original" });
             check.Nodes.Add(new TreeNode() { Select = "ohne Original" });
             treeList.Add(check);
 
-            //Zweig Ereignisprotokoll
+            //Treenode logfile              //Zweig Ereignisprotokoll
             Tree log = new Tree() { Headline = "Ereignisprotokoll" };
             treeList.Add(log);
 
@@ -56,24 +59,26 @@ namespace MediaConverter
         #region Properties
         public RecoveryOriginalView _currentView { get; set; }
         #endregion Properties
-
+        //command for the Selection if the user wants to recover, convert or check the files
+        //command für die Auswahl, ob der User wiederherstellen, konvertieren oder prüfen will
         #region Commands
-        private ICommand _ClickRecoveryOriginal;
-        public ICommand ClickRecoveryOriginal
+        private ICommand _ClickSelect;
+        public ICommand ClickSelect
         {
             get
             {
-                return _ClickRecoveryOriginal ?? (_ClickRecoveryOriginal = new RelayCommand<object>(x => { LoadUserControl(); }));
+                return _ClickSelect ?? (_ClickSelect = new RelayCommand<object>(x => { LoadUserControl(); })); //parameter fehlt noch
             }
         }
         #endregion Commands
 
         #region Methods
+        //Stringcompare of "select" to load the needed usercontrol
+        //Stringabgleich von "select" (Auswahl), der dann dem Content Binding sagt, welche Usercontrol geladen wird
         private void LoadUserControl()
         {
             
-
-          
+             //$Übergabe von select an Content$      
              MessageBox.Show("Hello");
 
         }
